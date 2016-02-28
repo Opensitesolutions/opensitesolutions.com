7cc9d4a5ceb6ee81371c25b616b674fccce566b1
Clarence Mills <cmills@opensitesolutions.com>
2016-02-28 14:09:51 -0500

    Central directory change

--- index.php
+++ index.php
@@ -1,259 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Open Site Solutions</title>
-<meta charset="utf-8">
-<meta name="format-detection" content="telephone=no" />
-<link rel="icon" href="images/favicon.ico">
-<link rel="shortcut icon" href="images/favicon.ico" />
-<link rel="stylesheet" href="css/style.css">
-<script src="js/jquery.js"></script>
-<script src="js/jquery-migrate-1.1.1.js"></script>
-<script src="js/jquery.easing.1.3.js"></script>
-<script src="js/script.js"></script> 
-<script src="js/superfish.js"></script>
-<script src="js/jquery.equalheights.js"></script>
-<script src="js/jquery.mobilemenu.js"></script>
-<script src="js/tmStickUp.js"></script>
-<script src="js/jquery.ui.totop.js"></script>
-<script>
- $(window).load(function(){
-  $().UItoTop({ easingType: 'easeOutQuart' });
-  $('#stuck_container').tmStickUp({});  
- }); 
-</script>
-<!--[if lt IE 8]>
- <div style=' clear: both; text-align:center; position: relative;'>
-   <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
-     <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
-   </a>
-</div>
-<![endif]-->
-<!--[if lt IE 9]>
-<script src="js/html5shiv.js"></script>
-<link rel="stylesheet" media="screen" href="css/ie.css">
-<![endif]-->
-</head>
-<body class="page1" id="top">
-<!--==============================
-              header
-=================================-->
-<header>
-  <div class="container">
-    <div class="row">
-      <div class="grid_12 rel">
-        <h1>
-          <a href="index.php">
-            <img src="images/logo.png" alt="Open Site Solutions">
-          </a>
-        </h1>
-      </div>
-    </div>
-  </div>
-  <section id="stuck_container">
-  <!--==============================
-              Stuck menu
-  =================================-->
-    <div class="container">
-      <div class="row">
-        <div class="grid_12 ">
-          <div class="navigation ">
-            <nav>
-              <ul class="sf-menu">
-               <li class="current"><a href="index.php">Home</a></li>
-               <li><a href="#about">About</a></li>
-               <li><a href="#services">Services</a></li>
-               <li><a href="#contact">Contact</a></li>
- 	           </ul>
-            </nav>
-            <div class="clear"></div>
-          </div>       
-         <div class="clear"></div>  
-        </div>
-     </div> 
-    </div> 
-  </section>
-  <section class="page1_header">
-    <div class="container">
-      <div class="row">
-        <div class="grid_4">
-          <a href="http://site.toolsforthecloud.com" target="_blank" class="banner "><div class="maxheight">
-            <div class="fa fa-globe"></div>Domain Registration</div>
-          </a>
-          <a href="http://www.fixyourip.com" target="_blank" class="banner "><div class="maxheight">
-            <div class="fa fa-lightbulb-o"></div>Network Solutions</div>
-          </a>
-          <a href="#services" class="banner "><div class="maxheight1">
-            <div class="fa fa-cog"></div>Services</div>
-          </a>
-          <a href="http://www.opensitesolutions.com/home.php" class="banner "><div class="maxheight1">
-            <div class="fa fa-briefcase"></div>OS Software</div>
-          </a>
-        </div>
-        <div class="grid_5">
-          <h2>Online <br> Solutions <br> Services</h2>
-          We help you run your business.
-        </div>
-      </div>
-    </div>
-  </section>
-</header>
-<a name="about"></a>
-<div class="block-1">
-  <div class="container">
-    <div class="row">
-      <div class="grid_3">
-        <div class="block-1_count"></div>
-        <a href="/home.php">Open Source <br> Applications</a>
-        <div class="clear"></div>
-      </div>
-      <div class="grid_3">
-        <div class="block-1_count"></div>
-        <a href="http://www.fixyourip.com">Online Network <br> Services</a>
-        <div class="clear"></div>
-      </div>
-      <div class="grid_3">
-        <div class="block-1_count"></div>
-        <a href="http://www.toolsforthecloud.com">Online Presence </a><br> 
-        <div class="clear"></div>
-      </div>
-      <div class="grid_3">
-        <!-- <a href="#" class="support"><img src="images/support.png" alt=""></a> -->
-       <div class="block-1_count"></div>
-        	<a href="google_apps_for_work.html">Google APPS for Work</a><br> 
-        <div class="clear"></div>
-      </div>
-    </div>
-  </div>
-</div>
-<!--=====================
-          Content
-======================-->
+<?php
+    include 'includes/header.inc';
+?>
 
-<section id="content"><div class="ic"></div>
-  <div class="container">
-    <div class="row">
-      <div class="grid_10 preffix_1 ta__center">
-        <div class="greet">
-          <h2 class="head__1">
-            <img src="images/logo-large.png" alt="Open Site Solutions">
-          </h2>
-          <p>Open Site Solutions consulting, recommending applications, solutions and integration of services and software to create unique solutions.  </p>
-		  <p>Open Site solutions on occasion uses Open Source applications as an alternative to commercial software, thereby lowering costs and maintaining quality service. Open source solutions is comprised of different software applications bundled together to create a unique and most times better solution than commercial software. 
-		  Open source is not freeware or shareware. Open Source software is built from a community of developers committed to a common goal who then make the source code and program available to download allowing modifications and contributions back into the source code. </p>
-          
-        </div>
-      </div>
-        <a name="services"></a> 
-    </div>
-  </div>
+<?php
+    include 'includes/topmenu.inc';
+?>
 
-  <article class="content_gray">
-    <div class="container">
-      <div class="row">
-        <div class="grid_7">
-          <h3>Services</h3>
-          <div class="block-2">
-            <img src="images/page1_img1.jpg" alt="" class="img_inner fleft">
-            <div class="extra_wrapper">
-              <div class="text1"><a href="http://www.toolsforthecloud.com" target="_blank">Cloud Business Tools</a></div>
-              <p>With nearly everyone using the web as their first source, getting your business online and into the cloud is a goal that is attainable. 
-              Tools for the cloud offers all the tools needed to get your company online very quickly and easily.
-              <br>
-              <a href="http://www.toolsforthecloud.com" target="_blank" class="link-1">more</a>
-            </div>
-          </div>
-          <div class="block-2">
-            <img src="images/page1_img2.jpg" alt="" class="img_inner fleft">
-            <div class="extra_wrapper">
-              <div class="text1"><a href="http://www.fixyourip.com" target="_blank">Online Network Tools</a></div>
-              <p>Providing tools to assist system, network administrators and the internet community to solve DNS, IP and network related problems.
-              Need to find out who owns a domain or IP address, use FixYourIP tools to create reports and resolve problems.
-              </p>
-              <a href="http://www.fixyourip.com" target="blank" class="link-1">more</a>
-            </div>
-          </div>
-        </div>
-        <div class="grid_4 preffix_1">
-          <h3>Testimonial</h3>
-          <blockquote class="bq1">
-            <p>“Hello! Great website you have!   Very useful!   Thank you ”</p>
-            <span>Matthew </span>
-          </blockquote>
-          <blockquote class="bq1">
-            <p>“Just wanted to drop a quick thanks for fixyourip.com. I use your online tools on a regular basis, since a corporate policy prevents me from SSH'ing into my own server and running dig/nmap/openssl from there. Your site has saved my butt on a regular basis.
-Have a good day, ”</p>
-            <span>Joel</span>
-          </blockquote>
-        </div>
-      </div>
-    </div>
-  </article>
-  <div class="container">
-    <div class="row">
-      <div class="grid_5">
-        <h4>Company</h4>
-        <img src="images/page1_img3.jpg" alt="" class="img_inner fleft">
-          <p>Providing consultation, tools and advice to resolve business related problems </p>
-          <p class="offset__1">While Striving to provide the right service and solution to meet your business needs</p>
-      </div>
-      <div class="grid_4">
-        <h4>Solutions</h4>
-        <ul class="list-1">
-          <li><a href="home.php">Open Source Solutions and applications</a></li>
-          <li><a href="http://www.toolsforthecloud.com" target="_blank">Cloud Solutions </a></li>
-          <li><a href="http://site.toolsforthecloud.com/hosting/web-hosting_4.aspx?ci=83725&prog_id=495959&pl_id=495959" target="_blank">Web Hosting</a></li>
-          <li><a href="http://site.toolsforthecloud.com/email/hosted-exchange.aspx?ci=58429&prog_id=495959&pl_id=495959" target="_blank">Email Services</a></li>
-          <li><a href="http://site.toolsforthecloud.com" target="_blank">Domain Registration</a></li>
-          <li><a href="http://site.toolsforthecloud.com/hosting/website-builder2.aspx?ci=1806&prog_id=495959" target="_blank">Website Builder</a>
-          <li><a href="http://www.fixyourip.com" target="_blank">Network Diagnostic Tools</a></li>
-          <li><a href="/index.php">Open source software solutions</a></li>
+<?php
+    include 'includes/right.inc';
+?>
+
+<!-- Center Column -->
+                <div id="primaryContentContainer">
+                        <div id="primaryContent">
+				<h2>Operating System - Alternative to Windows</h2>
+				<br><br>
+				<p>
+OpenSiteSolutions informing about integrators, hardware and Open Source solutions that can be used as an alternative to commercial software. Open source solutions are comprised of different software bundled together to create a unique and most times better solution than commercial software.
+</p>
+
+<p class="post align-right">
+        <ul>
+        <b>Open Source Alternative - BSD Based</b>
+        <li><a href="http://www.pcbsd.org" target="_blank">PC-BSD</a> Open Source Desktop oriented operating system based entirely on freebsd geared towards windows u
+sers looking for a secure reliable alternative to windows based systems.</li><br>
+        <li><a href="http://www.freesbie.org" target="_blank">Freesbie</a> based on freebsd but can be booted and run directly from the CD allowing trial of the softw
+are before you decide to install this small fast open source system. Also can be customized depending on your liking.</li><br>
+        <li><a href="http://www.desktopbsd.net" target="_blank">DesktopBSD</a> desktop oriented version based on freebsd combining the stability of freebsd with the e
+ase of use of windows avalible for AMD and i386 systems.</li><br>
+for educating users on freebsd</li><br>
+        <li><a href="http://www.reactos.org" target="_blank"> ReactOS</a> is an advanced free open source operating system providing a ground-up implementation of a M
+icrosoft Windows XP compatible operating system. ReactOS aims to achieve complete binary compatibility with both applications and device drivers meant
+ for NT and XP operating systems, by using a similar architecture and providing a complete and equivalent public interface.  ReactOS is the most compl
+ete working model of a Windows\xae like operating system available.</li><br>
+
         </ul>
-      </div>
-      <div class="grid_3">
-      	<a name="contact"></a> 
-        <h4>Contact</h4>
-        <address>
-          <ul class="cont_address">
-            <li>Toronto, ON, Canada</li>
-            <li>+1 647 749 5083</li>
-            <li><a href="#">consulting@opensitesolutions.com</a></li>
-            <li><a href="#">www.opensitesolutions.com</a></li>
-          </ul>
-        </address>
-      </div>
-    </div>
-  </div>
-</section>
-<!--==============================
-              footer
-=================================-->
-<footer id="footer">
-  <div class="container">
-    <div class="row">
-      <div class="grid_12"> 
-        <div class="copyright"><span class="brand">Open Site Solutions</span> &copy; <span id="copyright-year"></span> | <a href="#">Privacy Policy</a>
-          
-        </div>
-      </div>
-    </div>
-  </div>  
-</footer>
-<a href="#" id="toTop" class="fa fa-chevron-up"></a>
+</p>
 
-<!-- Start Google Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<p class="post align-right">
+        <ul>
+        <b>Open Source Alternative - Linux Based</b>
+	<li><a href="http://www.knoppix.net" target="_blank">knoppix</a> Free and Open Source Live Linux CD. Knoppix is a GNU/Linux distribution that boots and runs completely from cd. It includes recent linux software and desktop environments, with programs such as OpenOffice.org, Abiword, The Gimp, Konqueror, Mozilla, Apache, PHP, MySQL and hundreds of other quality open source programs.</li><br>
+	<li><a href="http://www.ubuntu.com/" target="_blank">Ubuntu</a> is a community developed, linux-based operating system that is perfect for laptops, desktops and servers. It contains all the applications you need - a web browser, presentation, document and spreadsheet software, instant messaging and much more. <a href="http://www.ubuntu.com/products/whatisubuntu" target="_blank"> learn more about ubuntu</a></li>
+	<br>
+	<b>Other Ubuntu releases</b>
+	<ul>
+	<li><a href="http://edubuntu.org/" target="_blank">Edubuntu</a></li>
+	<li><a href="http://xubuntu.org" target="_blank">Xubuntu</a></li>
+	<li><a href="http://www.kubuntu.org/" target="_blank">Kubuntu</a></li>
+	</ul>
+	<br>
+	<li><a href="http://linuxdigital.org//">Open Xange</a> is an open source desktop that was modeled from the ground up to look and feel like windows vista. A complete
+ and very easy to use operating system based on linux. A liveCD allows reviewing before installing</li><br>
+  </ul>
+</p>
 
-  ga('create', 'UA-744038-1', 'auto');
-  ga('send', 'pageview');
+                        </div>
+                </div>
+        <!-- End center Column -->
 
-</script>
 
-<!-- End Google Analytics -->
+<?php
+    include 'includes/left.inc';
+?>
 
-</body>
-</html>
+
+<?php
+    include 'includes/footer.inc';
+?>
