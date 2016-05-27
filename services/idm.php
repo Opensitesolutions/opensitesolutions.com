0d701ee69b890383bfb7abc8d086dc2dd94fb362
Clarence Mills <cmills@opensitesolutions.com>
2016-05-27 12:54:58 -0400

    Removed style.css and update meta description and keywords

--- services/idm.php
+++ services/idm.php
@@ -4,14 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content=''>
-    <meta name="keywords" content="">
+    <meta name="description" content='Create open source directory that can be used for authenticating users'>
+    <meta name="keywords" content="identity management, open source identity management, ldap, ldap configuration.">
     <title>Identitiy Management</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/styles.css" rel="stylesheet">
     <link href="/css/custom.css" rel="stylesheet">
 </head>
 <body>
